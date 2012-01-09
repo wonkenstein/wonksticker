6df0e875d34b6971bf9e36a24a67b265c05c9170
Mark Wong <marky.wong@gmail.com>
2012-01-09 22:28:15 +0000

    Adding vertical scrolling

--- wonksticker.plugin.js
+++ wonksticker.plugin.js
@@ -8,6 +8,7 @@
       speed : 30, // higher the number, the slower it is
       tickerBuffer : 30,
       width: 0,
+      height: 0,
       scroll: 'left',
     };
 
@@ -23,6 +24,7 @@
     this.speed = this.options.speed;
     this.tickerBuffer = this.options.tickerBuffer;
     this.tickerWidth = this.options.width;
+    this.tickerHeight = this.options.height;
     this.scroll = this.options.scroll;
 
     // captialise the scroll to use for function names
@@ -59,7 +61,12 @@
     mask.css('position', 'relative');
     mask.css('overflow', 'hidden');
 
-		this.horizontalTickerStyle(ticker, mask);
+		if (this.scroll == 'Up' || this.scroll == 'Down') {
+			this.verticalTickerStyle(ticker, mask);
+		}
+		else {
+			this.horizontalTickerStyle(ticker, mask);
+		}
 
     // hover behaviours and start scrolling
     var thisObj = this;
@@ -73,28 +80,26 @@
   };
 
 
-	// get the dimensions of the ticker
-	Plugin.prototype._getTickerDimensions = function(ticker) {
-		var thisObj = this;
+  // scroll direction taken from options.scroll
+  Plugin.prototype.startScrolling = function() {
+    var thisObj = this;
 
-    $('li', ticker).each(function(i){
-    	var w = thisObj._getElementWidth($(this));
-    	var h = $(this).height();
+    this.tickerInterval = window.setInterval(function () {
+    	if ($.isFunction(thisObj['scroll'+thisObj.scroll])) {
+    		thisObj['scroll'+thisObj.scroll]();
+    	}
 
-      thisObj.dimensions.tickerTapeLength += w;
-
-      if (thisObj.dimensions.itemMaxWidth < w) {
-      	thisObj.dimensions.itemMaxWidth = w;
-      }
-      if (thisObj.dimensions.itemMaxHeight < h) {
-      	thisObj.dimensions.itemMaxHeight = h;
-      }
-    });
-
-    thisObj.dimensions.tickerTapeLength += this.tickerBuffer // add on buffer so IE behaves
-	};
+    }, this.speed)
+  };
 
 
+  // stop/pause the ticker
+  Plugin.prototype.stopScrolling = function() {
+    window.clearInterval(this.tickerInterval);
+  };
+
+
+	// STYLE FUNCTIONS
 	// set up the styles for a horizontal ticker
 	Plugin.prototype.horizontalTickerStyle = function(ticker, mask) {
 
@@ -110,6 +115,22 @@
 	};
 
 
+	// set up styles for a vertical ticker
+	Plugin.prototype.verticalTickerStyle = function(ticker, mask) {
+		this._getTickerDimensions(ticker);
+
+		var tickerMaskHeight = (this.tickerHeight)
+											|| (this.dimensions.tickerTapeLength - (2*this.dimensions.itemMaxHeight));
+
+    ticker.css('height', this.dimensions.tickerTapeLength + 'px');
+
+    // set dimensions the ticker mask
+    mask.css('height', tickerMaskHeight + 'px');
+    mask.css('width', this.dimensions.itemMaxWidth + 10 + 'px');
+	}
+
+
+	// SCROLLING FUNCTIONS
   // scroll the ticker left
   Plugin.prototype.scrollLeft = function() {
     this.currPos += this.increment;
@@ -121,7 +142,7 @@
       this.currPos += this.dimensions.itemMaxWidth;
     }
 
-    // shift the ticker across right
+    // shift the ticker across left
     $(this.element).css("left", this.currPos + "px");
   };
 
@@ -142,25 +163,83 @@
   };
 
 
-  // scroll direction taken from options.scroll
-  Plugin.prototype.startScrolling = function() {
-    var thisObj = this;
+  // scroll the ticker up
+  Plugin.prototype.scrollUp = function() {
+    this.currPos += this.increment;
 
-    this.tickerInterval = window.setInterval(function () {
-      thisObj['scroll'+thisObj.scroll]();
-    }, this.speed)
+    // if first item has moved up enough then append it to end of list
+    if (this.currPos < (this.dimensions.itemMaxHeight * -1) - this.tickerBuffer) {
+      var item = $(this.element).children("li").eq(0).remove();
+      $(this.element).append(item);
+      this.currPos += this.dimensions.itemMaxHeight;
+    }
+
+    // shift the ticker across up
+    $(this.element).css("top", this.currPos + "px");
+  };
+
+  // scroll the ticker down
+  Plugin.prototype.scrollDown = function() {
+    this.currPos += this.increment;
+
+    // if first item has moved down enough then append it to end of list
+    if (this.currPos < ((this.dimensions.itemMaxHeight * -1) - this.tickerBuffer)) {
+      var item = $(this.element).children("li").last().remove();
+      $(this.element).prepend(item);
+      this.currPos += this.dimensions.itemMaxHeight;
+    }
+
+    // shift the ticker down
+    $(this.element).css("bottom", this.currPos + "px");
   };
 
 
-  //
-  Plugin.prototype.stopScrolling = function() {
-    window.clearInterval(this.tickerInterval);
-  };
 
-  // helper to get the wdth of an element including the margins
-  Plugin.prototype._getElementWidth= function(el) {
+
+	// DIMENSION FUNCTIONS
+	// get the dimensions of the ticker
+	Plugin.prototype._getTickerDimensions = function(ticker) {
+		var thisObj = this;
+
+    $('li', ticker).each(function(i){
+    	var w = thisObj._getElementWidth($(this));
+    	var h = thisObj._getElementHeight($(this));
+
+			if (thisObj.scroll == 'Up' || thisObj.scroll == 'Down') {
+				thisObj.dimensions.tickerTapeLength += h;
+			}
+			else {
+				thisObj.dimensions.tickerTapeLength += w;
+			}
+
+      if (thisObj.dimensions.itemMaxWidth < w) {
+      	thisObj.dimensions.itemMaxWidth = w;
+      }
+      if (thisObj.dimensions.itemMaxHeight < h) {
+      	thisObj.dimensions.itemMaxHeight = h;
+      }
+    });
+
+    if (this.scroll == 'Up' || this.scroll == 'Down') {
+    	this.tickerBuffer = parseInt(this.dimensions.itemMaxHeight/2);
+    }
+    else {
+	    this.tickerBuffer = parseInt(this.dimensions.itemMaxWidth/2);
+    }
+
+    thisObj.dimensions.tickerTapeLength += this.tickerBuffer // add on buffer so IE behaves
+	};
+
+  // helper to get the width of an element including the margins
+  Plugin.prototype._getElementWidth = function(el) {
   	el = $(el);
   	return el.outerWidth() + parseInt(el.css('marginLeft')) + parseInt(el.css('marginRight'));
+  };
+
+  // helper to get the height of an element including the margins
+  Plugin.prototype._getElementHeight = function(el) {
+  	el = $(el);
+  	return el.height() + parseInt(el.css('marginTop')) + parseInt(el.css('marginBottom'));
   };
 
 
